fefddc9c0356d23ec410324143f0b55bd02ea290
simran2195 <simran13104@iiitd.ac.in>
2021-01-22 11:12:55 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-
-$(document).ready(function() {
-	
-$('.menu , .linee').on('click', function() {
-  $('.menu').toggleClass('over')
-  $('.linea1').toggleClass('overL1')
-  $('.linea2').toggleClass('overL2')
-  $('.linea3').toggleClass('overL3')
-  $('.main-menu').toggleClass('overmain')
-  
-});
-$("html").niceScroll({cursorwidth: '8px',cursorcolor:"#222222" , cursorborder:"1px solid #FFF" , autohidemode: false, zindex: 9999 });
+
+
+$(document).ready(function() {
+
+	
+
+$('.menu , .linee').on('click', function() {
+
+  $('.menu').toggleClass('over')
+
+  $('.linea1').toggleClass('overL1')
+
+  $('.linea2').toggleClass('overL2')
+
+  $('.linea3').toggleClass('overL3')
+
+  $('.main-menu').toggleClass('overmain')
+  
+
+});
+
+$("html").niceScroll({cursorwidth: '8px',cursorcolor:"#222222" , cursorborder:"1px solid #FFF" , autohidemode: false, zindex: 9999 });
+
+});
+
+$(function(){ 
+    var navMain = $("#tohide");
+    navMain.on("click", "a", null, function () {
+      navMain.collapse('hide');
+    });
 });
